856d0b2d85c593f585fccdeb380746c9473adff9
alanh <alanh@DESKTOP-9NNGHJI>
2021-02-13 10:38:57 -0600

    RH

--- src/mx/edu/cesba/mx/Employee.java
+++ src/mx/edu/cesba/mx/Employee.java
@@ -7,15 +7,27 @@ package mx.edu.cesba.mx;
  */
 public class Employee {
 
+    //CAMPOS: CARACTERISTICAS DE UN OBJETO
     public int id;
     public String firstName;
     public String lastName;
     public int age;
     public int phoneNumber;
+    public String email;
     public double salary;
     public String hiredate;
 
-    public void getDetils() { //metodo
+    public void getDetils() { //METODO: ACCIONES QUE REALIZA UN OBJETO
+        
+        System.out.println("id: " +  id);
+        System.out.println("FirstName: " +   firstName);
+        System.out.println("LastName: " +   lastName);
+        System.out.println("age: " +   age);
+        System.out.println("email: "  +   email);
+        System.out.println("Phone Number: " +   phoneNumber);
+        System.out.println("Salary: " +   salary);
+        System.out.println("Hire Date: " +   hiredate);
+                
 
         
     }    
