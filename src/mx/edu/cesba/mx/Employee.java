bb0d22b4b87f937a9f86dc7b56fe1a6fb1c43ba0
alanh <alanh@DESKTOP-9NNGHJI>
2021-02-20 11:29:11 -0600

    WINDOW

--- src/mx/edu/cesba/mx/Employee.java
+++ src/mx/edu/cesba/mx/Employee.java
@@ -8,14 +8,79 @@ package mx.edu.cesba.mx;
 public class Employee {
 
     //CAMPOS: CARACTERISTICAS DE UN OBJETO
-    public int id;
-    public String firstName;
-    public String lastName;
-    public int age;
-    public int phoneNumber;
-    public String email;
-    public double salary;
-    public String hiredate;
+    private int id;
+    private String firstName;
+    private String lastName;
+    private int age;
+    private int phoneNumber;
+    private String email;
+    private double salary;
+    private String hiredate;
+    
+    //METODOS PARA ACCEDER A PRIVATE USANDO GET y SET
+    //ID 
+    public void setId(int id){
+        this.id= id;    
+    }
+    public int getId(){
+        return id;
+    }
+    //FIRST NAME
+    public void setfirstName(String firstName){
+        this.firstName= firstName;
+    }
+    public String getfirstName(){
+        return firstName;
+    }
+    //LAST NAME
+    public void setlastName(String lastName){
+        this.lastName= lastName;
+    }
+    public String getlastName(){
+        return lastName;
+    }
+    //AGE
+     public void setAge(int age){
+         if (age>=18 && age<65){
+             this.age= age;
+         }else{
+             System.err.println("Edad Invalida");
+         }
+            
+    }
+    public int getAge(){
+        return age;
+    }
+    //EMAIL
+    public void setEmail(String email){
+        this.email= email;
+    }
+    public String getEmail(){
+        return email;
+    }
+    //PHONENUMBER
+     public void setphoneNumber(int phoneNumber){
+        this.phoneNumber= phoneNumber;    
+    }
+    public int getphoneNumber(){
+        return phoneNumber;
+    }
+    //SALARY
+    public void setSalary(double salary){
+        this.salary= salary;    
+    }
+    public double getSalary(){
+        return salary;
+    }
+    //HIREDATE
+     public void sethireDate(String hiredate){
+        this.hiredate= hiredate;
+    }
+    public String gethireDate(){
+        return hiredate;
+    }
+    
+  
 
     public void getDetils() { //METODO: ACCIONES QUE REALIZA UN OBJETO
         
@@ -33,3 +98,4 @@ public class Employee {
         
     }    
 }
+   
