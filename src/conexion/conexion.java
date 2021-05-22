6bafe520baee1d785351b744a5b258dfe0c86ef2
alanh <alanh@DESKTOP-9NNGHJI>
2021-05-22 09:50:27 -0500

    version 1.6

--- src/conexion/conexion.java
+++ src/conexion/conexion.java
@@ -1,6 +1,5 @@
 /*
-ALAN DANIEL SEVILLA HERNANDEZ
-2022525
+LUIS EDUARDO RAMIREZ RIVAS 2022793
  */
 package conexion;
 
@@ -10,72 +9,80 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 
 public class conexion {
+ Connection con;
+ 
+ public conexion(){
+     
+     String driver = "com.mysql.jdbc.Driver";
+     String url = "jdbc:mysql://localhost:3306/hr";
+     
+     try{
+         Class.forName(driver);
+         con = DriverManager.getConnection(url, "root", "root");
+     }catch (Exception e){
+         
+     }
+ }
+ 
+ public Connection getConnection(){
+     
+     return con;
+     
+ }
     
     
-    public static void main(String [] args){
+    /*
+    public static void main(String[] args) {
         
-        //REQUERIMIENTOS
+        
+        
+        
+        
+ //requerimientos
+        //1.-REQUERIMIENTOS:
         String driver = "com.mysql.jdbc.Driver";
-        
-        
-        
-        //URL (CADENA DE CONEXION)
-        String url = "jdbc:mysql://localhost:3306/rh"; //ip: 198.201.33.05
-        
-        try{
+        // 2.- URL (cadena de conexión
+        String url = "jdbc:mysql://localhost:3306/hr";
+
+        try {
+
             Class.forName(driver);
             System.out.println("Conexion Exitosa");
-            
-        }catch(Exception e){
-            System.err.println("Error de conexion!!!" + e.getMessage());
+
+        } catch (Exception e) {
+            System.err.println("Error de Conexion !!" + e.getMessage());
             return;
         }
-        
-        try{
+
+        try { 
             Connection cn = DriverManager.getConnection(url, "root", "root");
-            System.out.println("Conectado a la base de datos");
-            System.out.println("----------------------");
-            
+            System.out.println("Conectado a la Base de DATOS");
+
+            System.out.println("----------------------------------------");
+
             String sql = "select * from employee";
             Statement st = cn.createStatement();
             ResultSet rs = st.executeQuery(sql);
-            
-           /* while(rs.next()){
-                
+
+            while (rs.next()) {
+
                 System.out.println(rs.getInt(1));
                 System.out.println(rs.getString(2));
                 System.out.println(rs.getString(3));
                 System.out.println(rs.getInt(4));
-                System.out.println(rs.getInt(5));
-                System.out.println(rs.getString(6));
+                System.out.println(rs.getString(5));
+                System.out.println(rs.getInt(6));
                 System.out.println(rs.getDouble(7));
                 System.out.println(rs.getString(8));
-                System.out.println("----------------------");
-    
+                System.out.println("-----------------------------------");
+
             }
-            */
-           while(rs.next()){
-                
-                System.out.println(rs.getInt("id"));
-                System.out.println(rs.getString("FirstName"));
-                System.out.println(rs.getString("LastName"));
-                System.out.println(rs.getInt("Age"));
-                System.out.println(rs.getInt("phonenumber"));
-                System.out.println(rs.getString("email"));
-                System.out.println(rs.getDouble("salary"));
-                System.out.println(rs.getString("hiredate"));
-                System.out.println("----------------------");
-    
-            }
-           
-           
-           
-           
-            
-        }catch(Exception e){
-            System.err.println("Sin Conexion a la base de datos");
+
+        } catch (Exception e) {
+            System.err.println("Sin conexion a la Base de Datos" + e.getMessage());
             return;
         }
-    }
 
+    }
+*/
 }
