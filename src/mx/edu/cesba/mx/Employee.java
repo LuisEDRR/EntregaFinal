71e1e87f8b5c724295539061c78eccf3fb05263e
alanh <alanh@DESKTOP-9NNGHJI>
2021-05-22 09:55:25 -0500

    version 2

--- src/mx/edu/cesba/mx/Employee.java
+++ src/mx/edu/cesba/mx/Employee.java
@@ -18,68 +18,7 @@ public class Employee {
     private String hiredate;
     
     //METODOS PARA ACCEDER A PRIVATE USANDO GET y SET
-    //ID 
-    public void setId(int id){
-        this.id= id;    
-    }
-    public int getId(){
-        return id;
-    }
-    //FIRST NAME
-    public void setfirstName(String firstName){
-        this.firstName= firstName;
-    }
-    public String getfirstName(){
-        return firstName;
-    }
-    //LAST NAME
-    public void setlastName(String lastName){
-        this.lastName= lastName;
-    }
-    public String getlastName(){
-        return lastName;
-    }
-    //AGE
-     public void setAge(int age){
-         if (age>=18 && age<65){
-             this.age= age;
-         }else{
-             System.err.println("Edad Invalida");
-         }
-            
-    }
-    public int getAge(){
-        return age;
-    }
-    //EMAIL
-    public void setEmail(String email){
-        this.email= email;
-    }
-    public String getEmail(){
-        return email;
-    }
-    //PHONENUMBER
-     public void setphoneNumber(int phoneNumber){
-        this.phoneNumber= phoneNumber;    
-    }
-    public int getphoneNumber(){
-        return phoneNumber;
-    }
-    //SALARY
-    public void setSalary(double salary){
-        this.salary= salary;    
-    }
-    public double getSalary(){
-        return salary;
-    }
-    //HIREDATE
-     public void sethireDate(String hiredate){
-        this.hiredate= hiredate;
-    }
-    public String gethireDate(){
-        return hiredate;
-    }
-    
+  
   
 
     public void getDetils() { //METODO: ACCIONES QUE REALIZA UN OBJETO
@@ -97,5 +36,69 @@ public class Employee {
 
         
     }    
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public String getHiredate() {
+        return hiredate;
+    }
+
+    public void setHiredate(String hiredate) {
+        this.hiredate = hiredate;
+    }
 }
    
